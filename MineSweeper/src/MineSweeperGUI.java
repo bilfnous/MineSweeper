50ff09922644ff93e4d74150bc9c7d531d083a46
B. Alfanous <b.alfanous@outlook.com>
2021-12-24 20:23:56 -0500

    Added emojis

--- MineSweeper/src/MineSweeperGUI.java
+++ MineSweeper/src/MineSweeperGUI.java
@@ -1,17 +1,11 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MineSweeperGUI extends Application {
@@ -28,10 +22,44 @@ public class MineSweeperGUI extends Application {
         
         Scene scene = new Scene(grid, 290, 350);
         
+        Label mines = new Label("010");
+        Label timeElapsed = new Label("00:00");
+
+        Image smilePNG = new Image("pics/smile.png");
+        ImageView smileIV = new ImageView(smilePNG);
+        smileIV.setFitHeight(30);
+        smileIV.setFitWidth(30);
+        Image coolPNG = new Image("pics/cool.png");
+        ImageView coolIV = new ImageView(coolPNG);
+        coolIV.setFitHeight(30);
+        coolIV.setFitWidth(30);
+        Image deadPNG = new Image("pics/dead.png");
+        ImageView deadIV = new ImageView(deadPNG);
+        deadIV.setFitHeight(30);
+        deadIV.setFitWidth(30);
+        Image minePNG = new Image("pics/mine.png");
+        ImageView mineIV = new ImageView(minePNG);
+        mineIV.setFitHeight(25);
+        mineIV.setFitWidth(25);
+        Image explosionPNG = new Image("pics/explosion.png");
+        ImageView explosionIV = new ImageView(explosionPNG);
+        explosionIV.setFitHeight(25);
+        explosionIV.setFitWidth(25);
+        Image flagPNG = new Image("pics/flag.png");
+        ImageView flagIV = new ImageView(flagPNG);
+        flagIV.setFitHeight(25);
+        flagIV.setFitWidth(25);
+
+        grid.add(mines, 2, 0);
+        grid.add(timeElapsed, 8, 0);
+        grid.add(smileIV, 5, 0);
+        
+
+
         btn.setMaxHeight(50);
         btn.setMinHeight(50);
-        for (int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; c++){
+        for (int r = 1; r < 10; r++){
+            for (int c = 1; c < 10; c++){
                 btn = new Button();
                 btn.setPrefSize(30, 30);
                 grid.add(btn,r,c);
